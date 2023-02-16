02841fbbcbaa293de99a7f732030f15d6c22daef
manibharthi <manibharthi@gmail.com>
2023-02-16 13:05:47 +0530

    add/auto/scaling

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import time
 print("hello world")
-
-# google-cloud-sdk\bin\dev_appserver.py "D:\Codeoverflow\google app engin"
+time.sleep(10)    
+# def executeSomething():
+#     #code here
+#     print("Hello world!")
+#     # time.sleep(1)
+    
+# while True:
+    # executeSomething()
+# google-cloud-sdk\bin\dev_appserver.py --enable_host_checking=False "D:\Codeoverflow\google\google app engin"
